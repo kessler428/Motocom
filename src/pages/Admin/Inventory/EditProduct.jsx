1ed406a25794f78734dc5bc3600147bf479685dc
kessler <kesslertorres497@gmail.com>
2022-08-24 20:14:55 -0600

    Adding credit veiw

--- src/pages/Admin/Inventory/EditProduct.jsx
+++ src/pages/Admin/Inventory/EditProduct.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const EditProduct = () => {
+  let { productId } = useParams();
   return (
     <div>EditProduct</div>
   )
