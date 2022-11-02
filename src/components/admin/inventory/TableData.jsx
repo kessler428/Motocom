fcc5757e5879a57982b42821b0822908bfdd914b
kessler <kesslertorres497@gmail.com>
2022-11-02 11:16:32 -0600

    last upload

--- src/components/admin/inventory/TableData.jsx
+++ src/components/admin/inventory/TableData.jsx
@@ -1,129 +1,159 @@
-// import { PaginationInventory } from "./Pagination";
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from "react-redux";
 
-import DataTable from 'react-data-table-component';
-import { useState } from 'react';
-import { GridSearchBar } from '../GridSearch';
+import { FaPencilAlt } from "react-icons/fa";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { deleteProductById } from "../../../redux/slices/inventory/thunks";
+import Swal from "sweetalert2";
+import { setIsLoading } from "../../../redux/slices/ui/uiSlices";
+import { useNavigate } from "react-router-dom";
 
-import { FaPencilAlt } from 'react-icons/fa';
-import { RiDeleteBin5Line } from 'react-icons/ri';
-import { deleteProductById } from '../../../redux/slices/inventory/thunks';
-import Swal from 'sweetalert2';
-import { setIsLoading } from '../../../redux/slices/ui/uiSlices';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import pedidos from "../../../img/pedidos.png";
 
+import DataTable from "react-data-table-component";
+import DataTableExtensions from 'react-data-table-component-extensions';
+import 'react-data-table-component-extensions/dist/index.css';
+import { useFetch } from "../../../hooks/useFetch";
+import { SpinerLoading } from "../../SpinnerLoading";
 
 export const TableData = () => {
   const dispatch = useDispatch();
   const Navigate = useNavigate();
 
-  const [data, setData] = useState([]);
-
-  const { listInventory } = useSelector((state) => state.inventory);
-  const [searchProduct, setSearchProduct] = useState('');
+  const { datos, loading } = useFetch(`almacen`);
 
   const deleteProduct = (id) => {
     Swal.fire({
-      title: '¿Esta Seguro de eliminar este producto?',
-      icon: 'warning',
-      confirmButtonText: 'Si',
+      title: "¿Esta Seguro de eliminar este producto?",
+      icon: "warning",
+      confirmButtonText: "Si",
       showDenyButton: true,
-      denyButtonText: 'Cancelar'
+      denyButtonText: "Cancelar",
     }).then((result) => {
       if (result.isConfirmed) {
-        dispatch(deleteProductById(id))
-        dispatch(setIsLoading(true))
+        dispatch(deleteProductById(id));
+        dispatch(setIsLoading(true));
       } else if (result.isDenied) {
-        Swal.fire('', 'No se elimino el producto.!', 'info')
+        Swal.fire("", "No se elimino el producto.!", "info");
       }
-    })
-  }
+    });
+  };
 
   const editProduct = (id) => {
     Navigate(`edit_product/${id}`);
-  }
-
-  useEffect(() => {
-    if(listInventory.length > 0) {
-      const columns = [
-        {
-          name: "Nombre",
-          selector: row => row.nombreArticulo
-        },
-        {
-          name: "Codigo 1",
-          selector: row => row.codigoUno
-        },
-        {
-          name: "Marca",
-          selector: row => row.marca
-        },
-        {
-          name: "Modelo",
-          selector: row => row.modelo
-        },
-        {
-          name: "Precio de compra",
-          cell: row => <p>C${parseFloat(row.precioCompra).toLocaleString('us-Us')}</p>
-        },
-        {
-          name: "Precio de venta",
-          cell: row => <p>C${parseFloat(row.precioVenta).toLocaleString('us-Us')}</p>
-        },
-        {
-          name: "Almacen Principal",
-          selector: row => row.stock?.[0].stock
-        },
-        {
-          name: "Camion Marlon",
-          selector: row => row.stock?.[1].stock
-        },
-        {
-          name: "Camion Miguel",
-          selector: row => row.stock?.[2].stock
-        },
-        {
-          name: "Camion Pokemon",
-          selector: row => row.stock?.[3].stock
-        },
-        {
-          name: "Acciones",
-          cell: row =>
-            <div className='flex flex-row gap-4 justify-start w-80'>
-              <button className='text-yellow-400 hover:bg-gray-200 p-3 rounded-full' onClick={ () => editProduct(row.id) }><FaPencilAlt className='w-5 h-5'/></button>
-              <button className='text-red-700 hover:bg-gray-200 p-3 rounded-full' onClick={ () => deleteProduct(row.id) }><RiDeleteBin5Line className='w-5 h-5'/></button>
-            </div>
-        },
-      ]
-
-      setData(columns);
-    }
-  }, [listInventory])
-  
-
-
-  const filteredItems = listInventory.filter(
-		item => item.nombreArticulo.includes(searchProduct) || item.codigoUno.includes(searchProduct) || item.modelo.includes(searchProduct),
-	);
-
-  const paginationComponentOptions = {
-    rowsPerPageText: 'Filas por página',
-    rangeSeparatorText: 'de',
-    selectAllRowsItem: true,
-    selectAllRowsItemText: 'Todos',
   };
 
-  return (
-    <>
-      <GridSearchBar searchProduct={searchProduct} setSearchProduct={setSearchProduct}/>
+  const columns = [
+    {
+      name: "Nombre",
+      selector: (row) => row.nombreArticulo,
+    },
+    {
+      name: "Codigo 1",
+      selector: (row) => row.codigoUno,
+    },
+    {
+      name: "Marca",
+      selector: (row) => row.marca,
+    },
+    {
+      name: "Modelo",
+      selector: (row) => row.modelo,
+    },
+    {
+      name: "Precio de compra",
+      cell: (row) => (
+        <p>C${parseFloat(row.precioCompra).toLocaleString("us-Us")}</p>
+      ),
+    },
+    {
+      name: "Precio de venta",
+      cell: (row) => (
+        <p>C${parseFloat(row.precioVenta).toLocaleString("us-Us")}</p>
+      ),
+    },
+    {
+      name: "Tienda principal",
+      selector: (row) => row.stock?.[0]?.stock,
+    },
+    {
+      name: "Tienda 2",
+      selector: (row) => row.stock?.[1]?.stock,
+    },
+    {
+      name: "Acciones",
+      cell: (row) => (
+        <div className="flex flex-row gap-4 justify-start w-80">
+          <button
+            className="text-yellow-400 hover:bg-gray-200 p-3 rounded-full"
+            onClick={() => editProduct(row.id)}
+          >
+            <FaPencilAlt className="w-5 h-5" />
+          </button>
+          <button
+            className="text-red-700 hover:bg-gray-200 p-3 rounded-full"
+            onClick={() => deleteProduct(row.id)}
+          >
+            <RiDeleteBin5Line className="w-5 h-5" />
+          </button>
+        </div>
+      ),
+    },
+  ];
 
-      <DataTable
-      columns={data}
-      data={ filteredItems }
-      pagination
-      paginationComponentOptions={paginationComponentOptions}
-      />
+  const paginationComponentOptions = {
+    rowsPerPageText: "Filas por página",
+    rangeSeparatorText: "de",
+    selectAllRowsItem: true,
+    selectAllRowsItemText: "Todos",
+  };
+
+  const tableData = {
+    columns,
+    data: datos?.almacenes,
+  }
+
+  return loading ? <SpinerLoading /> : (
+    <>
+    {
+      datos?.almacenes.length === 0 ? (
+          <div className="flex flex-col mt-20 justify-center">
+            <div className="flex justify-center">
+                <img
+                    src={pedidos}
+                    alt=""
+                    className="h-32 bg-gray-100 rounded-full p-4 w-32"
+                />
+            </div>
+            <div className="flex justify-center">
+                <p className="text-gray-600 font-bold">
+                    No hay productos en stock
+                </p>
+            </div>
+        </div>
+      ) : (
+        <div className="bg-white mt-6 p-4">
+          <DataTableExtensions
+            {...tableData}
+            filterPlaceholder="Buscar un producto"
+            filter={true}
+            pagination={true}
+            paginationComponentOptions={paginationComponentOptions}
+            print={false}
+            export={false}
+          >
+            <DataTable
+              noHeader
+              style={{width: '100%', marginTop: '2rem'}}
+              defaultSortField="id"
+              pagination
+              defaultSortAsc={false}
+              paginationComponentOptions={paginationComponentOptions}
+            />
+          </DataTableExtensions>
+        </div>
+
+      )
+    }
     </>
   );
 };
